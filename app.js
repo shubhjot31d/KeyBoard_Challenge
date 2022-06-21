cc4be4b16eebaa653f39e8a54abc894d02157240
Shubhjot <shubhjotsinghd31@gmail.com>
2022-06-21 10:41:20 +0530

    Addition of jigggling chracters and recognising the charracters

--- app.js
+++ app.js
@@ -6,13 +6,37 @@ const shift_btn = document.querySelector('.shift')
 const space_btn = document.querySelector('.space')
 
 let chars = []
+var check;
+function generateLetter() {
+    var possible = "abcdefghijklmnopqrstuvwxyz"; 
+       check = possible.charAt(Math.floor(Math.random() * possible.length));
+    return check;
+  }
 
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        textarea.value += btn.innerText
-        chars = textarea.value.split('')
-    })
-})
+function timer(){
+    var n = textarea.value.length;
+    let blink = textarea.value.en;
+    blink.style.opacity=blink.style.opacity == 0?1:0;
+    setTimeout(timer,500);
+}
+
+
+    var  temp = generateLetter();
+    textarea.value += temp;
+
+
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            var k = btn.innerText;
+            if(k===temp){
+                temp=generateLetter();
+                textarea.value += temp;               
+            }
+    
+            chars = textarea.value.split('')
+        })
+    })   
 
 delete_btn.addEventListener('click', () => {
     chars.pop()
